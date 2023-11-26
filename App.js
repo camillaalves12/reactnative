c1d9b9dfdec2f0c9bdc252e636571114a7f39c46
camilla alves <camillaalves285@gmail.com>
2023-11-26 19:31:21 -0300

    Adicionei mais componentes

--- App.js
+++ App.js
@@ -4,17 +4,41 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.div}>
+        <Text style={{fontSize:'2rem'}}>UFAL </Text>
+        <StatusBar style="auto" />
+
+          <Text style={styles.text}>SISTEMAS DE INFORMAÇÃO</Text>
+          <Text style={styles.text}>DISPOSITIVOS MÓVEIS</Text>
+          <Text style={styles.text}>7° PERÍODO</Text>
+      </View>
     </View>
+
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1E90FF',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: '2rem',
+    
   },
+  div: {
+    borderWidth: '0.1rem',
+    padding: '1rem',
+    borderRadius: '2rem',
+    alignItems: 'flex-start',
+    display: 'flex',
+    flexDirection: 'row'
+  },
+
+  text: {
+    color: '#1C1C1C',
+    fontSize: '1.2rem',
+    padding: '0.5rem'
+  }
 });
